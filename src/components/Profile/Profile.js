8621263603527d82aea63a358b95f74543d34a19
Viktoria Vishniakova <vikaveter1563@yandex.ru>
2022-07-13 01:01:05 +0300

    added form to profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -9,16 +9,31 @@ const Profile = () => {
             <Header />
             <div className="profile">
             
-            <h2 className="profile__heading">Привет, Виталий!</h2>
-            <div className="profile__container">
-                <p className="profile__container-heading">Имя</p>
-                <p className="profile__text">qwerty</p>
-            </div>
-            <div className="profile__container">
-                <p className="profile__container-heading">E-mail</p>
-                <p className="profile__text">qwerty</p>
-            </div>
-            <p className="profile__edit">Редактировать</p>
+                <h2 className="profile__heading">Привет, Виталий!</h2>
+                <form className="profile-form">
+                    <div className="profile__container">
+                        <label className="profile__label">Имя</label>
+                        <input
+                            className="profile__input"
+                            type="text"
+                            required
+                            name="name"
+                            maxLength="30"
+                            minLength="2"
+                            value="Qwerty">
+                        </input>
+                    </div>
+                    <div className="profile__container">
+                        <label className="profile__label">E-mail</label>
+                        <input className="profile__input"
+                            type="emaio"
+                            required
+                            name="email"
+                            value="Qwerty@qwerty.q">
+                        </input>
+                    </div>
+                    <button className="profile__edit">Редактировать</button>
+                </form>
             <Link className="profile__logout" to="/signin">Выйти из аккаунта</Link>
 
         </div>
